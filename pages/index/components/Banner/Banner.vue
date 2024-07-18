3def70066c34beed65ba6d425f8d57982ac507b9
my峰 <1137022356@qq.com>
2024-07-18 16:37:55 +0800

    lunbo

--- pages/index/components/Banner/Banner.vue
+++ pages/index/components/Banner/Banner.vue
@@ -1,16 +1,34 @@
 <script setup>
   import { ref } from "vue"
+  import { getBannerApi } from "../../../../servers";
+  
+  const users = ref([])
+
+  const getBanner = async () => {
+	const res = await getBannerApi()
+	users.value = res.data.banners
+	console.log(res,users.value);
+  }
+  getBanner()
   
   
 </script>
 
 <template>
-  <view>
-    轮播
-  </view>
+<van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+  <van-swipe-item v-for="(item,index) in users" :key="index" ><img :src="item.imageUrl" style="width: 375px;"/></van-swipe-item>
+</van-swipe>
 </template>
 
 
 <style lang="scss">
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+	height: 139px;
+    line-height: 139px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
 
 </style>
