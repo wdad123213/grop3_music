e35ef3fed1070aec395b6f89f1ddfc24afca222d
吴江 <2731105140@qq.com>
2024-07-18 11:24:57 +0800

    start

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,27 +1,15 @@
+<script setup>
+  import { ref } from 'vue'
+	const title = ref('网易云')
+</script>
+
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
 	</view>
 </template>
-
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
-</script>
 
 <style>
 	.content {
@@ -30,21 +18,6 @@
 		align-items: center;
 		justify-content: center;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
