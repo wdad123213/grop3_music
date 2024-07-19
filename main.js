c54aaee2bdc4914140eafad6196bd0e46140c641
my峰 <1137022356@qq.com>
2024-07-19 21:04:01 +0800

    轮播修改

--- main.js
+++ main.js
@@ -2,21 +2,10 @@ import App from './App'
 import * as Pinia from 'pinia';
 import { createSSRApp } from 'vue'
 
-import { 
-  Swipe,
-  SwipeItem
- } from 'vant';
-// 2. 引入组件样式
- import 'vant/lib/index.css';
-
-
-
 
 export function createApp() {
   const app = createSSRApp(App)
   app.use(Pinia.createPinia())
-  app.use(Swipe)
-  app.use(SwipeItem)
   return {
     app,
     Pinia
