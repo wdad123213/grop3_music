70d5a73db179db011063dc1785d669e3cb3358b4
姚科旭 <137674068@qq.com>
2024-07-19 22:32:13 +0800

    a

--- App.vue
+++ App.vue
@@ -14,4 +14,21 @@
 
 <style>
 	/*每个页面公共css */
+	@import url("./colorui/icon.css");
+	@import url("./colorui/main.css");
+	/*每个页面公共css */
+	page{
+		width: 100%;
+		height: 100%;
+	}
+	.content{
+		width: 100%;
+		height: 100%;
+		background-color: #FFFFFF;
+		color: #000000;
+		box-sizing: border-box;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+	}
 </style>
